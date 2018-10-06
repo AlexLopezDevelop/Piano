2913795ab5c483252e8c308a67d34985dc7ee85c
Cristian Lopez <alex_lopez.es@icloud.com>
2018-10-06 19:42:04 +0200

    Finished

--- Piano/ViewController.swift
+++ Piano/ViewController.swift
@@ -7,14 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let soundsArray = ["c1", "c1s", "d1", "d1s", "e1", "f1", "f1s", "g1", "g1s", "a1", "a1s", "b1", "c2"]
+    
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func playSound(_ sender: UIButton) {
+        let idkey = sender.tag
+        let fileName = soundsArray[idkey-1]
+        
+        if let soundURL : URL = Bundle.main.url(forResource: fileName, withExtension: "mp3") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            } catch {
+                print(error)
+            }
+            
+            audioPlayer.play()
+        }
+    }
+    
 }
 
